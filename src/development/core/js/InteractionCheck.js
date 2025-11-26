3b1d87f8d92507f5923aafc2578da277b1e224dd
rtlehr <itsme@rosslehr.com>
2025-11-26 16:00:04 -0500

    added interactionCheck

---
+++ src/development/core/js/InteractionCheck.js
@@ -0,0 +1,30 @@
+class InteractionCheck {
+
+    constructor(course) 
+    {
+
+    this.course = course;
+
+    }
+
+    checkNotViewed()
+    {
+
+        if($(".notViewed").length > 0)
+        {
+
+        }
+        
+    }
+
+    addClickCheck()
+    {
+
+    }
+
+    checkInteractions()
+    {
+
+    }
+
+}
